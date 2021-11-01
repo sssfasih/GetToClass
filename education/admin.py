e31356350466438572da7718dd9cc78e7492d150
Syed Fasih Uddin <sssfasih@gmail.com>
2021-11-01 22:12:31 +0500

    added dashboard

--- education/admin.py
+++ education/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import User, UserImage
 # Register your models here.
+
+
+admin.site.register(UserImage)
+
+class UserImageInline(admin.TabularInline):
+    model = UserImage
+    extra = 3
+
+class PropertyAdmin(admin.ModelAdmin):
+    inlines = [ UserImageInline, ]
+
+admin.site.register(User, PropertyAdmin)
